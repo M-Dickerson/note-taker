54aa7b01de8feec19398ba3a689ffaa1fb3cc85c
D <Dickerson.Morgan@outlook.com>
2022-12-12 19:44:25 -0500

    added middleware and more comments

--- server.js
+++ server.js
@@ -10,12 +10,18 @@ const port = 3001;
 // activates express
 const app = express();
 
+// needed middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// gets information from notes and index files
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+// reads the file and returns data
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
