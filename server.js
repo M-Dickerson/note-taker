7ef946b1b19275b246c1c8d2d9e8835efad20075
D <Dickerson.Morgan@outlook.com>
2022-12-12 21:47:31 -0500

    fixed a typo where utf-8 is

--- server.js
+++ server.js
@@ -2,50 +2,60 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
-// generates unique ids
-const uuid = require("./helpers/uuid");
-// port
-const PORT = 3001;
-// express
+const util = require('util');
+// port, uuid, and app
+const PORT = process.env.PORT || 3001;
+const uuid = require('./helpers/uuid');
 const app = express();
 
 // needed middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // gets information from notes and index files
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // reads the file and returns data
-app.get("/api/notes", (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-  app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add notes`);
   
     const { title, text } = req.body;
   
-    if (req.body) {
+    if (title && text) {
       const newNote = {
         title,
         text,
         id: uuid(),
       };
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        } else {
+          const notes = JSON.parse(data);
+          notes.push(newNote);
 
+          fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, `\t`), (err) =>
+            err
+            ? console.error(err)
+            : console.info(`You did it!`)
+          );
+        }
+      })
     const response = {
-        status: "You did it!",
-        body: newNote,
+    status: "You did it!",
+    body: newNote,
     };
 
     console.log(response);
@@ -54,6 +64,6 @@ app.get("/api/notes", (req, res) => {
     res.status(500).json("Error");}
   });
 // logs where the server is being hosted
-  app.listen(PORT, () =>
-  console.log(`Server is at http://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Blah listening on port http://localhost:${PORT}`)
 );
