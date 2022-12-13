5fc09b98f2c20fa55d544f6dc0d9ad515305d4e7
D <Dickerson.Morgan@outlook.com>
2022-12-12 23:58:31 -0500

    fixed a typo on listen port command. Organized the code

--- server.js
+++ server.js
@@ -2,33 +2,29 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require('util');
+const util = require("util");
 // port, uuid, and app
 const PORT = process.env.PORT || 3001;
-const uuid = require('./helpers/uuid');
+const uuid = require("./helpers/uuid");
 const app = express();
 
 // needed middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// gets information from notes and index files
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+// gets information from notes file
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // reads the file and returns data
-app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+app.get("/api/notes", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
   });
 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add notes`);
   
     const { title, text } = req.body;
@@ -63,7 +59,11 @@ app.post('/api/notes', (req, res) => {
     } else {
     res.status(500).json("Error");}
   });
+// THIS HAS TO BE AT THE BOTTOM
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 // logs where the server is being hosted
 app.listen(PORT, () =>
-  console.log(`Blah listening on port http://localhost:${PORT}`)
+  console.log(`App is listening on port http://localhost:${PORT}`)
 );
