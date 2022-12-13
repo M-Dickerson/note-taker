f8b7a4abb5b83cebad3aba2aaf2b59ca3f69ba9c
D <Dickerson.Morgan@outlook.com>
2022-12-12 20:16:26 -0500

    added const response as well as statuses for 201 and 500

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-// links
+// required links DO NOT ERASE
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
-// generates ids
+// generates unique ids
 const uuid = require("./helpers/uuid");
 // port
-const port = 3001;
-// activates express
+const PORT = 3001;
+// express
 const app = express();
 
 // needed middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 // gets information from notes and index files
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
@@ -21,8 +22,38 @@ app.get("/notes", (req, res) =>
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
 // reads the file and returns data
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
   });
+
+  app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add notes`);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+
+    const response = {
+        status: "You did it!",
+        body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+    } else {
+    res.status(500).json("Error");}
+  });
+// logs where the server is being hosted
+  app.listen(PORT, () =>
+  console.log(`Server is at http://localhost:${PORT}`)
+);
